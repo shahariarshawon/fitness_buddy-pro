66340dfb8376acb093a1f1cb09d59800778c5ab0
shahariarshawon <shahariararafat7739@gmail.com>
2026-06-01 10:10:20 +0600

    added auth midleware

---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) => {
+  try {
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      res.status(401);
+      throw new Error("Not authorized. No token provided");
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      res.status(401);
+      throw new Error("User not found");
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.status(401);
+    next(new Error("Not authorized. Token failed"));
+  }
+};
+
+module.exports = {
+  protect,
+};
