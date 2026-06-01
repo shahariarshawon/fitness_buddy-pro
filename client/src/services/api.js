ef6c7d1d842c4a545ccf7d8099dc99d56cc2bc79
shahariarshawon <shahariararafat7739@gmail.com>
2026-06-01 12:38:43 +0600

    added services api

---
+++ client/src/services/api.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Add token automatically to protected requests
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("kynorafit_token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Auto logout if token is invalid/expired
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("kynorafit_token");
+      localStorage.removeItem("kynorafit_user");
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
